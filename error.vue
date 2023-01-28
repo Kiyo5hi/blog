caec2c73f1a2da5dcfa3f33752407291f0994028
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-01-28 05:27:20 +0000

    chore(*): added 404 page

---
+++ error.vue
@@ -0,0 +1,22 @@
+<template>
+  <div
+    class="min-h-screen bg-slate-200 dark:bg-slate-900 dark:text-white text-lg
+  flex flex-col gap-2 items-center justify-center"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-16 h-16"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
+      />
+    </svg>
+    <div>404 Absolutely Nothing Found</div>
+  </div>
+</template>
