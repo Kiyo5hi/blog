956618d0c437bf6ef7ea2bb7ede1c665e59f41ef
Kiyo5hi <44930252+Kiyo5hi@users.noreply.github.com>
2023-04-24 19:30:09 +0000

    build(docker): build docker image on push

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-pnpm build
-rm -rf /home/coder/Source/srv/blog.k1yoshi.com/*
-cp -rf .output/* /home/coder/Source/srv/blog.k1yoshi.com
-
-echo 'Successfully deployed!'
